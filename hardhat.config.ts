b4c8135e8baed355a072c609d71bae3e7950557c
CanvasL <746591811@qq.com>
2023-02-21 13:23:29 +0800

    feat: hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const MNEMONIC_PHRASE = process.env.MNEMONIC_PHRASE || '';
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || '';
+
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545/"
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: {
+        mnemonic: MNEMONIC_PHRASE,
+        path: "m/44'/60'/0'/0",
+      }
+    }
+  }
 };
 
 export default config;
